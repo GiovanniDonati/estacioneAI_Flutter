4d717f3cf42b614e3b1be4d2815de6229701b8ff
GiovanniDonati <grdonati99@gmail.com>
2025-04-30 06:09:23 -0300

    Widgets components to otimization of system.

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:estacioneai/shared/widgets/textLabel.dart';
+
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
 
@@ -10,9 +12,33 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController nameController = TextEditingController();
+    TextEditingController bornController = TextEditingController();
+
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.green,),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextLabel(text: "Nome Completo"),
+              TextField(controller: nameController),
+              const SizedBox(height: 10,),
+              TextLabel(text: "Data de Nascimento"),
+              TextField(controller: bornController, readOnly: true, onTap: () async {
+                var data =  await showDatePicker(
+                  context: context, 
+                  firstDate: DateTime(1900), 
+                  lastDate: DateTime(2050));
+                if(data != null){
+                  bornController.text = "${data.day}/${data.month}/${data.year}";
+                }
+              }),
+              TextButton(onPressed: (){}, child: Text("Salvar")),
+            ],
+          ),
+        ),
     ));
   }
 }
